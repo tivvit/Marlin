42a8090a99cc234197d8f7f78bb00764343f9511
Vit Listik <tivvit@seznam.cz>
2018-11-26 17:58:53 +0100

    measurements

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -806,8 +806,8 @@
  *    (0,0)
  */
 // TODO(Volca): Measure these!
-#define X_PROBE_OFFSET_FROM_EXTRUDER 35  // X offset: -left  +right  [of the nozzle]
-#define Y_PROBE_OFFSET_FROM_EXTRUDER 10  // Y offset: -front +behind [the nozzle]
+#define X_PROBE_OFFSET_FROM_EXTRUDER 22  // X offset: -left  +right  [of the nozzle]
+#define Y_PROBE_OFFSET_FROM_EXTRUDER 5  // Y offset: -front +behind [the nozzle]
 #define Z_PROBE_OFFSET_FROM_EXTRUDER -0.2   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
